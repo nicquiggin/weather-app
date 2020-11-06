9681d113b6d4c0eaf7793bbc4392f96059ccd6b9
nicquiggin <71301277+nicquiggin@users.noreply.github.com>
2020-11-06 11:28:24 -0700

    Built 3 hour forecast, HTML and JS
    Everything works apart from searchLocation function, which does not call displayForecast function for some reason. To investigate!

--- src/script.js
+++ src/script.js
@@ -49,6 +49,9 @@ function searchLocation(position) {
   let apiKey = "79432bad8a08e24accbb2ab649dcc7be";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&units=${unit}&appid=${apiKey}`; 
   axios.get(apiUrl).then(displayWeatherConditions);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
@@ -57,7 +60,6 @@ function getCurrentLocation(event) {
 }
 
 function displayWeatherConditions(response) {
-  console.log(response.data);
   actualCelsiusTemperature = response.data.main.temp;
   feelsLikeCelsiusTemperature = response.data.main.feels_like;
   document.querySelector("#today-temp").innerHTML = Math.round(actualCelsiusTemperature); 
@@ -74,10 +76,50 @@ function displayWeatherConditions(response) {
   celsiusLink.classList.add("active");
 }
 
+function formatForecastHours(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+  hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+  minute = `0${minute}`;
+  }
+  return `${hour}:${minute}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatForecastHours(forecast.dt * 1000)}
+      </h3>
+      <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="${forecast.weather[0].description} />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+          / ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>`
+  }
+
+}
+
 function searchCity(city) {
   let apiKey = `79432bad8a08e24accbb2ab649dcc7be`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(displayWeatherConditions);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
